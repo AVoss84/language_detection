169bc0ca01d986c256484e9dd9b7245b83fd5d93
AVoss84 <a.vosseler@gmx.net>
2020-12-29 12:13:30 +0100

    start flask dev

--- app.py
+++ app.py
@@ -1,53 +1,16 @@
+# app.py - a minimal flask api using flask_restful
+from flask import Flask
+from flask_restful import Resource, Api
 
-from pathlib import Path
-import random, sys, os
-#from collections import Counter, defaultdict
-import numpy as np
-import pandas as pd
-from copy import deepcopy
-import yaml, pickle
-import warnings
-#from importlib import reload
-from utils import read, clean_text
+app = Flask(__name__)
+api = Api(app)
 
-# Surpress warnings due to different sklearn versions
-# used for saving and loading
-warnings.filterwarnings("ignore")
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-#filepath = Path.cwd() / 'data'     # wikipedia language identification data 2018 
+api.add_resource(HelloWorld, '/')
 
-# Import glossary for acronyms:
-#--------------------------------
-with open('glossary.yaml') as f:    
-    glossary = yaml.load(f, Loader=yaml.FullLoader)
-
-# Load the model from disk
-#----------------------------
-filename = 'trained_model.pkl'
-loaded_model = pickle.load(open(filename, 'rb'))
-print('Model loaded.')
-
-#----------------
-# Test sentence:
-#----------------
-new_sent = "Gestern abend ging ich in ein Restaurant. da saß ein Typ neben mir und meinte nur!"   # german
-new_sent = "Yesterday I was in the city. The weather was super nice."     # eng
-new_sent = "Вчера был в городе. Погода была очень хорошей."       # russian
-new_sent = "Ayer estuve en la ciudad. El clima estuvo super agradable."   # spanish
-new_sent = "Hier, j'étais en ville. Le temps était super beau."      # french
-new_sent = "Heri eram in civitate. Super tempestas est delicatus."     # latin
-new_sent = "Ieri ero in città. Il tempo è stato bellissimo."     # italian
-new_sent = "Igår var jag i staden. Vädret var supertrevligt."    # sweden
-new_sent = "ฉันอยากบอกสามีของฉันว่าฉันอยากได้แหวนเพชร ฮี่ฮี่ฮี่"     # if not working check result of preprocessing!!
-
-# Predict
-#---------
-fore = loaded_model.predict(pd.Series([new_sent]))[0]
-print('Prediction: {}'.format(glossary['label_desc'].get(fore, 'Unknown')))
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
